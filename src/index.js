fffa4d0931b842fe728cb81401f48acfdd8ea681
Pablo <ortizromeropablo3@gmail.com>
2023-01-05 15:54:27 -0300

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -6,48 +6,76 @@ function createElement(tagName, options){
   return Object.assign(document.createElement(tagName), options);
 }
 
-function dataHandler(data){
-    //Display main info about the given word
-    const wordHeader = createElement('h1', {innerText:`${data[0]['word']}`})
-    const wordType = createElement('p', {
-      innerText:` ${data[0]['meanings'][0].partOfSpeech}`
-    })
-    const audioElement = createElement('audio', {
-      controls:true,
-      src:`${data[0]["phonetics"].find(element => element.audio).audio}`
-    });
+function displayMainInfo(data){
+  //Display main info about the given word
+  const wordHeader = createElement('h1', {innerText:`${data[0]['word']}`})
+  const wordType = createElement('p', {
+    innerText:` ${data[0]['meanings'][0].partOfSpeech}`
+  })
+  const audioElement = createElement('audio', {
+    controls:true,
+    src:`${data[0]["phonetics"].find(element => element?.audio)?.audio}`
+  });
 
-    resultsBoard.append(wordHeader, wordType, audioElement, createElement('hr'));
+  resultsBoard.append(wordHeader, wordType, audioElement, createElement('hr'));
+}
 
-    //Diplay definitions and examples
-    for (const element of data){
-      element["meanings"][0]["definitions"].forEach(element => {
-        const definitionHeader = createElement('h3', {innerText:`${element.definition}`});
-        resultsBoard.append(definitionHeader);
-
-        if (!element.example){
-            resultsBoard.append(createElement('hr'));
-            return
-        }
-  
-        const listExamples = createElement('ul');
-        const exampleItem = createElement('li', {innerText: `${element.example}`});
-        listExamples.append(exampleItem);
-        resultsBoard.append(listExamples, createElement('hr'));
+function displayDefinitions(data) {
+  for (const element of data) {
+    element.meanings.forEach((meaning) => {
+      meaning.definitions.forEach((definition) => {
+        displayDefinition(definition);
       });
+    });
+  }
+}
+
+function displayDefinition(definition) {
+  const definitionHeader = createElement('h3', {innerText: `${definition.definition}`});
+  resultsBoard.append(definitionHeader);
+
+  if (!definition.example) {
+      resultsBoard.append(createElement('hr'));
+      return;
+  }
+
+  const listExamples = createElement('ul');
+  displayExample(definition.example, listExamples);
+  resultsBoard.append(listExamples, createElement('hr'));
+}
+
+function displayExample(example, list) {
+  const exampleItem = createElement('li', {innerText: `${example}`});
+  list.append(exampleItem);
+}
+
+function dataHandler(data){
+    try {
+      displayMainInfo(data)
+      displayDefinitions(data)
+    } catch (error) {
+      console.log(error)
     }
 }
 
 function displayError(error){
-    resultsBoard.append(error)
+    resultsBoard.append(error);
 }
 
-function fetchData(urlApi){
-    fetch(urlApi)
-    .then((response) => response.json())
-    .then((data) => dataHandler(data))
-    .catch((error) => displayError(error));
+async function fetchData(urlApi) {
+  try {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+
+    if (!response.ok){
+      throw new Error("Invalid WORD or Networking error!");
+    }
+    dataHandler(data);
+  } catch (error) {
+    displayError(error);
+  }
 }
+
 
 //main function
 function initSearch(event){
